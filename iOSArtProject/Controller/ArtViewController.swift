725ae8ea94cd9b48608af0ecd29fe2cfd0a5a182
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2018-11-01 11:54:31 -0600

    started project where we will learn to draw on screen

---
+++ iOSArtProject/Controller/ArtViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  iOSArtProject
+//
+//  Created by Fitzgerald, Skyler on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ArtViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+
+}
+
