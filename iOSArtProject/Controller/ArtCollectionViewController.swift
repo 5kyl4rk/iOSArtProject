37894d04af4e3eaf24f7fcba6a0aa0a5df07f6c9
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2018-11-07 13:22:39 -0700

    gave labels text, made methods that give proper spacing of cells and images
    I have no clue what any of those methods mean though, just copied it from my teacher

--- iOSArtProject/Controller/ArtCollectionViewController.swift
+++ iOSArtProject/Controller/ArtCollectionViewController.swift
@@ -36,14 +36,14 @@ public class ArtCollectionViewController: UICollectionViewController
     private let labels : [String] =
     {
         return [
-            "MeowthSample",
-            "CatSample",
-            "JavaSample",
-            "MainframeSample",
-            "SwiftSample",
-            "codeSample",
-            "continueSample",
-            "wizardSample"
+            "beta Meowth",
+            "my Octocat",
+            "Haiku about Java",
+            "Master teh Mainframe",
+            "Swift",
+            "art I made using LOGO",
+            "a e s t h e t i c",
+            "[A wandering WIZARD CAT has appeared!]"
         ]
     }()
     
@@ -81,15 +81,14 @@ public class ArtCollectionViewController: UICollectionViewController
 
     public override func numberOfSections(in collectionView: UICollectionView) -> Int
     {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        
+        return creativeCS.count
     }
 
     public override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
@@ -102,6 +101,32 @@ public class ArtCollectionViewController: UICollectionViewController
     }
 
     // MARK: UICollectionViewDelegate
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRowCompact + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRowCompact
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               insetForSectionAt section: Int) -> UIEdgeInsets
+    {
+        return sectionInsets
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               minimumLineSpacingForSectionAt section: Int) ->CGFloat
+    {
+        return sectionInsets.left
+    }
 
     /*
     // Uncomment this method to specify if the specified item should be highlighted during tracking
